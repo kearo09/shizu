0c0d6015e1eb653339894d999ebf602e4b4bfb57
NIKHIL KEARO <nikhilkairo9@gmail.com>
2025-07-20 06:13:57 +0900

    Update db.py

--- db.py
+++ db.py
@@ -3,8 +3,15 @@ import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
-DATABASE_URL = os.getenv("DATABASE_URL")
 def get_connection():
-    conn = psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
-    print("✅ DB connected")
-    return conn
+    try:
+        conn = psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
+        print("✅ DB connected")
+        with conn.cursor() as cur:
+            cur.execute("SHOW client_encoding;")
+            enc = cur.fetchone()
+            print("Client encoding from DB:", enc)
+        return conn
+    except Exception as e:
+        print("DB connection failed:", e)
+        raise
