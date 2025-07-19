4bb7a19f537bbccfaeac16395b0268ed8f1331df
NIKHIL KEARO <nikhilkairo9@gmail.com>
2025-07-20 06:07:29 +0900

    Update db.py

--- db.py
+++ db.py
@@ -4,9 +4,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
 def get_connection():
     conn = psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
-    with conn.cursor() as cur:
-        cur.execute("SET client_encoding TO 'UTF8';")
+    print("✅ DB connected")
     return conn
